4cab6318a1aed299e798a4b6b27f688c09fc0188
sergiolysenko <54682953+sergiolysenko@users.noreply.github.com>
2019-10-08 21:13:08 +0300

    minimize css and js

---
+++ js/booking-compressed.js
@@ -0,0 +1,1 @@
+var bookingHeader=document.querySelector(".booking__label"),formField=document.querySelector(".form__field");bookingHeader.addEventListener("click",function(e){e.preventDefault(),formField.classList.contains("form__field__hide")?formField.classList.remove("form__field__hide"):formField.classList.add("form__field__hide")});
