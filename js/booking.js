604c9edc9afd1316d90b6ea4a4535df459d64e43
sergiolysenko <54682953+sergiolysenko@users.noreply.github.com>
2019-10-08 08:09:00 +0300

    PROJECT COMPLETE
    Завершил полностью верстку проекта

---
+++ js/booking.js
@@ -0,0 +1,12 @@
+var bookingHeader = document.querySelector(".booking__label");
+var formField = document.querySelector(".form__field");
+
+bookingHeader.addEventListener("click", function (evt) {
+	evt.preventDefault();
+	if (formField.classList.contains("form__field__hide")) {
+		formField.classList.remove("form__field__hide")
+	}
+	else {
+		formField.classList.add("form__field__hide")
+	}
+});
