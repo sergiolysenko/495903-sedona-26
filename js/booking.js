532e46cc65e487935c9db3843632d022e73df5ba
sergiolysenko <54682953+sergiolysenko@users.noreply.github.com>
2019-10-09 22:03:04 +0300

    fixed a personal project
    Исправил Б.11, 17; Д. 8, 9, 20, 21

--- js/booking.js
+++ js/booking.js
@@ -1,6 +1,8 @@
 var bookingHeader = document.querySelector(".booking__label");
 var formField = document.querySelector(".form__field");
 
+formField.classList.add("form__field__hide");
+
 bookingHeader.addEventListener("click", function (evt) {
 	evt.preventDefault();
 	if (formField.classList.contains("form__field__hide")) {
